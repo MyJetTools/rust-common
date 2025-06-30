84de9637e2435022cdcfa7c532dd848d5cfb4d46
amigin <amigin@gmail.com>
2025-06-30 11:25:32 +0300

    Markdown applier

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #[cfg(feature = "country-code")]
 pub mod country_code;
+mod markdown_applier;
 #[cfg(feature = "validators")]
 pub mod validators;
+pub use markdown_applier::*;
---
+++ src/markdown_applier.rs
@@ -0,0 +1,161 @@
+use std::{cell::Cell, str::Chars};
+
+enum MarkDownToPush {
+    BrOpen,
+    BrClose,
+}
+
+impl MarkDownToPush {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            MarkDownToPush::BrOpen => "<br>",
+            MarkDownToPush::BrClose => "</br>",
+        }
+    }
+}
+
+pub struct MarkdownApplier<'s> {
+    text: &'s str,
+    chars: Chars<'s>,
+    pos: Cell<usize>,
+    from_pos: usize,
+    bold_open: bool,
+    prev_char: Option<char>,
+    push_markdown_to_push: Option<MarkDownToPush>,
+}
+
+impl<'s> MarkdownApplier<'s> {
+    pub fn new(text: &'s str) -> Self {
+        Self {
+            text,
+            chars: text.chars(),
+            pos: Cell::new(0),
+            from_pos: 0,
+            bold_open: true,
+            prev_char: None,
+            push_markdown_to_push: None,
+        }
+    }
+
+    pub fn get_next(&mut self) -> Option<&'s str> {
+        if let Some(markdown_applier) = self.push_markdown_to_push.take() {
+            return Some(markdown_applier.as_str());
+        }
+
+        let mut pos = self.pos.get();
+
+        if pos >= self.text.len() {
+            return None;
+        }
+        loop {
+            let next_char = self.chars.next();
+
+            if next_char.is_none() {
+                let result = &self.text[self.from_pos..pos];
+                self.pos.set(pos + 1);
+                return Some(result);
+            }
+
+            let next_char = next_char.unwrap();
+
+            if next_char == '*' {
+                if let Some(prev_char) = self.prev_char {
+                    if prev_char == '*' {
+                        if self.bold_open {
+                            self.push_markdown_to_push = Some(MarkDownToPush::BrOpen);
+                        } else {
+                            self.push_markdown_to_push = Some(MarkDownToPush::BrClose);
+                        }
+
+                        self.bold_open = !self.bold_open;
+
+                        let result = &self.text[self.from_pos..pos - 1];
+
+                        self.pos.set(pos + 1);
+                        self.from_pos = pos + 1;
+
+                        return Some(result);
+                    }
+                }
+            }
+
+            self.prev_char = Some(next_char);
+
+            pos += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::markdown_applier::MarkdownApplier;
+
+    #[test]
+    fn apply_markdown_text() {
+        let text = "1. **Text Title** Other text **TextTitle2**";
+
+        let mut markdown_applier = MarkdownApplier::new(text);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("1. ", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("<br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("Text Title", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("</br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!(" Other text ", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("<br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("TextTitle2", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("</br>", item);
+
+        assert!(markdown_applier.get_next().is_none())
+    }
+
+    #[test]
+    fn apply_markdown_text_with_text_at_the_end() {
+        let text = "1. **Text Title** Other text **TextTitle2** Other text2";
+
+        let mut markdown_applier = MarkdownApplier::new(text);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("1. ", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("<br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("Text Title", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("</br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!(" Other text ", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("<br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("TextTitle2", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!("</br>", item);
+
+        let item = markdown_applier.get_next().unwrap();
+        assert_eq!(" Other text2", item);
+
+        assert!(markdown_applier.get_next().is_none())
+    }
+}
