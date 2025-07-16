a39216e5f04ed1d62af1366b06b60ca8ccb5bd3b
amigin <amigin@gmail.com>
2025-07-16 13:13:28 +0300

    Performance improvements

--- src/markdown_applier/img_detector.rs
+++ src/markdown_applier/img_detector.rs
@@ -57,26 +57,25 @@ impl ImgDetector {
                             if index_alt_start <= index_url_end {
                                 let src = &self.value[index_url_start..index_url_end];
 
-                                let mut src = src.split(' ');
+                                let space_index = src.find(' ');
+
+                                let (left, right) = if let Some(space_index) = space_index {
+                                    (&src[..space_index], Some(&src[space_index + 1..]))
+                                } else {
+                                    (src, None)
+                                };
 
                                 out.push_str("<img src=\"");
-                                out.push_str(src.next().unwrap());
+                                out.push_str(left);
                                 out.push('"');
 
-                                if let Some(title) = src.next() {
-                                    let mut title = title.to_string();
-
-                                    while let Some(other_segment) = src.next() {
-                                        title.push(' ');
-                                        title.push_str(other_segment);
-                                    }
-
+                                if let Some(title) = right {
                                     if title.starts_with('"') {
                                         out.push_str(" title=");
-                                        out.push_str(title.as_str());
+                                        out.push_str(title);
                                     } else {
                                         out.push_str(" title=\"");
-                                        out.push_str(title.as_str());
+                                        out.push_str(title);
                                         out.push('"');
                                     }
                                 }
