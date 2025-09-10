041094e88493cc9d83d3b6cb44c7cc6b5d978248
amigin <amigin@gmail.com>
2025-09-10 19:35:56 +0200

    Hide Markdown behind markdown

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 #[cfg(feature = "country-code")]
 pub mod country_code;
-
+#[cfg(feature = "markdown")]
 mod markdown_applier;
 #[cfg(feature = "time-zones")]
 pub mod time_zones;
 #[cfg(feature = "validators")]
 pub mod validators;
+#[cfg(feature = "markdown")]
 pub use markdown_applier::*;
 
 #[cfg(feature = "fuzzy_data")]
 pub mod fuzzy_data;
+#[cfg(feature = "markdown")]
 pub mod markdown_segments;
 
 #[cfg(feature = "placeholders")]
